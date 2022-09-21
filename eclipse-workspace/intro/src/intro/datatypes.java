113e03c5045ebd91da902000576466762e2a8d08
Okaeanos <gurkanyildiz@dijitalgen.com>
2022-09-21 20:00:33 +0300

    Java 1.gün
    Kodlama io java kamp 1.gün

---
+++ eclipse-workspace/intro/src/intro/datatypes.java
@@ -0,0 +1,21 @@
+package intro;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		
+		double sayi =12.5;
+        sayi =-129;
+
+
+        int sayi2=1556;
+        sayi=1548;
+
+        String sehir ="ANKARA";
+        char karakter ='A';
+
+        boolean dogrumu = false;
+
+	}
+
+}
---
+++ eclipse-workspace/intro/src/intro/variables.java
@@ -0,0 +1,14 @@
+package intro;
+
+public class variables {
+
+	public static void main(String[] args) {
+
+		int ogrenciSayisi = 17 ;
+		String mesaj = "Öğrenci Sayısı : " ;
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+	}
+
+}
